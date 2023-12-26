88ead3b54494c525d7cec449b69f17e9a73a5170
herdeybayor <shereefdeen4real@gmail.com>
2023-12-26 12:43:32 +0100

    Add wireframe and deltaTime logging

--- lessons/05-animations/src/script.js
+++ lessons/05-animations/src/script.js
@@ -10,6 +10,7 @@ const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({
     color: 0xff0000,
+    wireframe: true,
 });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
@@ -40,6 +41,7 @@ const tick = () => {
     // Time
     const currentTime = Date.now();
     const deltaTime = currentTime - time;
+    console.log(deltaTime);
     time = currentTime;
 
     // Update objects
