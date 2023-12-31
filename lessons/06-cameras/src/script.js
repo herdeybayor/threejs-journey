25e594f3406616e4ce4521768ce2a503476d5043
herdeybayor <shereefdeen4real@gmail.com>
2023-12-31 10:38:53 +0100

    chore: update camera position and rotation based on cursor movement

--- lessons/06-cameras/src/script.js
+++ lessons/06-cameras/src/script.js
@@ -20,13 +20,13 @@ const mesh = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1, 5, 5, 5), new THREE.M
 scene.add(mesh);
 
 // Camera
-// const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
-// const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 100);
-const aspectRatio = sizes.width / sizes.height;
-const camera = new THREE.OrthographicCamera(-1 * aspectRatio, 1 * aspectRatio, 1, -1, 0.1, 100);
-camera.position.x = 2;
-camera.position.y = 2;
-camera.position.z = 2;
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
+// const aspectRatio = sizes.width / sizes.height;
+// const camera = new THREE.OrthographicCamera(-1 * aspectRatio, 1 * aspectRatio, 1, -1, 0.1, 100);
+
+// camera.position.x = 2;
+// camera.position.y = 2;
+camera.position.z = 3;
 camera.lookAt(mesh.position);
 scene.add(camera);
 
@@ -36,6 +36,19 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(sizes.width, sizes.height);
 
+// Cursor
+const cursor = {
+    x: 0,
+    y: 0,
+};
+
+window.addEventListener("mousemove", (event) => {
+    cursor.x = event.clientX / sizes.width - 0.5;
+    cursor.y = -(event.clientY / sizes.height - 0.5);
+
+    // console.log(cursor.x, cursor.y);
+});
+
 // Animate
 const clock = new THREE.Clock();
 
@@ -44,6 +57,12 @@ const tick = () => {
 
     // Update objects
     mesh.rotation.y = elapsedTime;
+
+    // Update camera
+    camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
+    camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
+    camera.position.y = cursor.y * 5;
+    camera.lookAt(mesh.position);
 
     // Render
     renderer.render(scene, camera);
